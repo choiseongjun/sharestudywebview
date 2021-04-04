672a187c1adeacc6faa3c9565ea692c06d869626
최성준 <csj2702@naver.com>
2021-04-04 18:17:11 +0900

    1.두번 누르면 앱종료(5초내) 추가

--- App.js
+++ App.js
@@ -1,42 +1,63 @@
-import React, { Component } from "react";
-import { BackHandler } from "react-native";
+import React,{useRef,useEffect,useState} from "react";
+import { BackHandler,SafeAreaView,View } from "react-native";
 import { WebView } from "react-native-webview";
 
-export default class App extends Component {
-  WEBVIEW_REF = React.createRef();
 
-  state = {
-    canGoBack: false,
-  };
- 
-  componentDidMount() {
-    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
-  }
+const App = () => {
+  const [exitApp,SETexitApp]=useState(false)
 
-  componentWillUnmount() {
-    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
-  }
+  const webview = useRef(null);
 
-  handleBackButton = () => {
-    if (this.state.canGoBack) {
-      this.WEBVIEW_REF.current.goBack();
-      return true;
+  const backAction = () => {
+    if(exitApp==false){
+        SETexitApp(true);
+        // Toast.show({
+        //   text2: '한번 더 뒤로가기 버튼을 누르면 종료됩니다.',
+        //   topOffset: 640,
+        //   visibilityTime: 500,
+        // });
+        onAndroidBackPress();
+        
     }
-  };
+    else if(exitApp==true){
+        BackHandler.exitApp()
+    }
 
-  onNavigationStateChange = (navState) => {
-    this.setState({
-      canGoBack: navState.canGoBack,
-    });
+    setTimeout(()=>{
+        SETexitApp(false)
+    }, 500);
+    return true;
   };
+  const onAndroidBackPress = () => {
+    if (webview.current) {
+      webview.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+  useEffect(()=> {
+    BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    };
+  }, []); // Never re-run this effect
 
-  render() {
-    return (
+  useEffect(() =>
+    {
+        const backHandler = BackHandler.addEventListener(
+            "hardwareBackPress",
+            backAction
+        );
+        return () => backHandler.remove();
+    },[exitApp]) 
+  return (
+    <SafeAreaView style={{ flex: 1,zIndex:0,backgroundColor:'transparent'}}>
       <WebView
-        source={{ uri: "http://3.35.255.192" }}
-        ref={this.WEBVIEW_REF}
-        onNavigationStateChange={this.onNavigationStateChange}
+        source={{ uri: 'http://3.36.73.41' }}
+        ref={webview}
       />
-    );
-  }
+    </SafeAreaView>
+  )
 }
+
+export default App
