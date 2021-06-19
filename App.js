b6f7514f73d9080a53ce20765b3b053ad7cf436a
최성준 <csj2702@naver.com>
2021-06-19 20:13:02 +0900

    1.fcm토큰 작업 수정

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React,{useRef,useEffect,useState} from "react";
-import { BackHandler,SafeAreaView,View,ScrollView,RefreshControl,Alert } from "react-native";
+import { BackHandler,SafeAreaView,View,ScrollView,RefreshControl,Alert,Text } from "react-native";
 import { WebView } from "react-native-webview";
 import messaging from '@react-native-firebase/messaging';
 import Cookie from 'react-native-cookie';
+import axios from 'axios';
 
 const wait = (timeout) => {
   return new Promise(resolve => setTimeout(resolve, timeout));
@@ -14,6 +15,7 @@ const App = () => {
   const [refreshY,setRefreshY] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
   const [token,setToken] = useState("");
+  const [url,setUrl] = useState("");
 
 
   let baseUrl = "http://3.36.73.41/"
@@ -46,7 +48,7 @@ const App = () => {
       authStatus === messaging.AuthorizationStatus.PROVISIONAL;
   
     if (enabled) {
-      console.log('Authorization status:', authStatus);
+      // console.log('Authorization status:', authStatus);
     }
   }
   const onAndroidBackPress = () => {
@@ -56,10 +58,12 @@ const App = () => {
     }
     return false;
   };
+
+
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       //Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-      Alert.alert(JSON.stringify(remoteMessage.notification))
+      //Alert.alert(JSON.stringify(remoteMessage.notification))
       
       //console.log(remoteMessage.notification);
     });
@@ -76,6 +80,7 @@ const App = () => {
       })
 
   },[])
+ 
 
   messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log('Message handled in the background!', remoteMessage);
@@ -106,12 +111,37 @@ const App = () => {
   }, [refreshY]); 
 
   useEffect(() => {
-    Cookie.set(baseUrl, 'FCM_TOKEN', token).then(() => null);
+    Cookie.set(baseUrl+"Login", 'FCM_TOKEN', token).then((res) => {
+      
+        axios.get("http://3.35.255.192:9090/api/auth/device/token",
+        {
+          params: {
+            token: token
+          }
+      
+        }
+        )
+        .then((res)=>{
+          console.log('res==',res)
+        })
+        
+      
+    }
+    );
+
     
   },[token])
+
+  
+
+  const _onNavigationStateChange = (event) =>{
+    setUrl(event.url)
+    
+  }
+
   return (
     <SafeAreaView style={{ flex: 1,zIndex:0,backgroundColor:'transparent'}}>
-      <ScrollView
+        <ScrollView
           contentContainerStyle={{ flex: 1,
             }}
           refreshControl={
@@ -125,35 +155,50 @@ const App = () => {
         <WebView
           source={{ uri: baseUrl }}
           ref={webview}
-          injectedJavaScript="
-            (function() {
-            function wrap(fn) {
-            return function wrapper() {
-              var res = fn.apply(this, arguments);
-              window.ReactNativeWebView.postMessage(window.location.href);
-              return res;
-            }
-            }
-            history.pushState = wrap(history.pushState);
-            history.replaceState = wrap(history.replaceState);
-            window.addEventListener('popstate', function() {
-              window.ReactNativeWebView.postMessage(window.location.href);
-            });
-            document.querySelector('.main_container').addEventListener('touchmove', function() {
-              ReactNativeWebView.postMessage(JSON.stringify(window.scrollY))
-            })
-            })();
-            true;   
-          "
-          onMessage={event => {
-             setRefreshY(event.nativeEvent.data)    
           
-          
-            }}
+          onNavigationStateChange={(event)=>_onNavigationStateChange(event)}
+
+          injectedJavaScript={INJECTED_JAVASCRIPT}
+          onMessage={(event)=> console.log(event.nativeEvent)}
+
         />
       </ScrollView>
+     
     </SafeAreaView>
   )
 }
+const INJECTED_JAVASCRIPT = `(function() {
+  function wrap(fn) {
+  return function wrapper() {
+    var res = fn.apply(this, arguments);
+    window.ReactNativeWebView.postMessage(window.location.href);
+    return res;
+  }
+  }
+  history.pushState = wrap(history.pushState);
+  history.replaceState = wrap(history.replaceState);
+  window.addEventListener('popstate', function() {
+    window.ReactNativeWebView.postMessage(window.location.href);
+  });
+  document.querySelector('.main_container').addEventListener('touchmove', function() {
+    ReactNativeWebView.postMessage(JSON.stringify(window.scrollY))
+  })
+  })();
+  true; 
+  `;
+
+
 
 export default App
+// 
+// 
+// `
+
+// function sayHi(who, phrase) {
+//   ReactNativeWebView.postMessage(JSON.stringify(window.document.querySelector(#userid)))
+// }
+
+// setTimeout(sayHi, 1000)
+    
+    
+// `;
